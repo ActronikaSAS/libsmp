75a1cc08655b63110127051e4a3cbfa569e39ab7
Aurélien Zanelli <aurelien.zanelli@actronika.com>
2019-10-09 14:50:36 +0200

    message: only shift by 16 when size_t size is greater than 2 in next_pow2()
    on AVR, size_t size is 2 bytes, thus trying to shift by 16 is undefined.

--- src/message.c
+++ src/message.c
@@ -380,7 +380,9 @@ static size_t next_pow2(size_t v)
     v |= v >> 2;
     v |= v >> 4;
     v |= v >> 8;
+#if SMP_SIZE_T_SIZE > 2
     v |= v >> 16;
+#endif
 #if SMP_SIZE_T_SIZE > 4
     v |= v >> 32;
 #endif
